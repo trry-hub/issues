7e1972c8e632e69bb4643625e87a8736283d7d4c
shizhu <shizhu@yaomaitong.cn>
2024-07-26 12:03:59 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,25 @@
 <script setup lang="ts">
 import eruda from 'eruda'
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
+import { onMounted } from 'vue';
+import Player from 'xgplayer'
+import 'xgplayer/dist/index.min.css';
 
 eruda.init()
+
+onMounted(() => {
+  const player = new Player({
+    id: 'player',
+    url: 'http://s2.pstatp.com/cdn/expire-1-M/byted-player-videos/1.0.0/xgplayer-demo.mp4',
+  })
+})
 </script>
 
 <template>
-  <div>
-    <button class="button" @click="router.push({ name: 'page1' })">page1</button>
-    <button class="button" @click="router.push({ name: 'page2' })">page2</button>
-    <hr>
-    <router-view v-slot="{ Component, route }">
-      <transition name="fade" mode="out-in" appear>
-        <component :is="Component" :key="route.fullPath" />
-      </transition>
-    </router-view>
-  </div>
+  <div id="player"></div>
 </template>
 
-<style scoped>
-.button {
-  height: 2em;
-  will-change: filter;
-  margin-right: 10px;
-  transition: filter 300ms;
-}
-
-/* 主内容区动画 */
-.fade-enter-active,
-.slide-left-enter-active,
-.slide-right-enter-active,
-.slide-top-enter-active,
-.slide-bottom-enter-active {
-  transition: 0.2s;
-}
-
-.fade-leave-active,
-.slide-left-leave-active,
-.slide-right-leave-active,
-.slide-top-leave-active,
-.slide-bottom-leave-active {
-  transition: 0.15s;
-}
-
-.fade-enter-from {
-  opacity: 0;
-}
-
-.fade-leave-to {
-  opacity: 0;
+<style lang="scss" scoped>
+#player{
+  width: 100%;
 }
 </style>
