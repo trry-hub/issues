4a7882bb89b3025755ed0d32041e0aa810a6b7e7
shizhu <shizhu@yaomaitong.cn>
2025-09-16 22:09:23 +0800

    refactor: 移除路由并添加 ECharts 图表
    - 移除了 vue-router 相关代码和依赖
    - 添加了 echarts 依赖
    - 在 App.vue 中实现了两个柱状图展示
    - 删除了 page1.vue 和 page2.vue 文件
    - 更新了 main.ts，移除了路由的引用

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,90 @@
 <script setup lang="ts">
-import eruda from 'eruda'
-import { useRouter } from 'vue-router'
+import { onMounted, ref } from 'vue'
+import * as echarts from 'echarts'
 
-const router = useRouter()
+const option = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      axisTick: {
+        alignWithLabel: true
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value'
+    }
+  ],
+  series: [
+    {
+      name: 'Direct',
+      type: 'bar',
+      barWidth: '60%',
+      data: [10, 52, 200, 334, 390, 330, 220]
+    }
+  ]
+}
+let chartInstance: null | echarts.ECharts = null
+const chartInstanceVueRef = ref()
+const chartDom = ref()
+const chartDomUseRef = ref()
 
-eruda.init()
+// 渲染图表
+async function renderChart() {
+  if (!chartInstance) {
+    chartInstance = echarts.init(chartDom.value)
+  }
+  chartInstance.setOption(option)
+}
+
+async function renderChartUseRef() {
+  if (!chartInstanceVueRef.value) {
+    chartInstanceVueRef.value = echarts.init(chartDomUseRef.value)
+  }
+  chartInstanceVueRef.value.setOption(option)
+}
+// 生命周期
+onMounted(() => {
+  renderChart()
+  renderChartUseRef()
+})
 </script>
 
 <template>
-  <div>
-    <button class="button" @click="router.push({ name: 'page1' })">page1</button>
-    <button class="button" @click="router.push({ name: 'page2' })">page2</button>
-    <hr>
-    <router-view v-slot="{ Component, route }">
-      <transition name="fade" mode="out-in" appear>
-        <component :is="Component" :key="route.fullPath" />
-      </transition>
-    </router-view>
+  <div class="chart-container" style="border-color: green;">
+    不使用 ref 接收变量
+    <div ref="chartDom" />
+  </div>
+  <div class="chart-container" style="border-color: red;">
+    使用 ref 接收变量
+    <div ref="chartDomUseRef" />
   </div>
 </template>
-
-<style scoped lang="scss">
-.button {
-  height: 2em;
-  will-change: filter;
-  margin-right: 10px;
-  transition: filter 300ms;
+<style scoped>
+.chart-container {
+  margin-bottom: 10px;
+  border-width: 1px;
+  border-style: solid;
+  width: 500px;
+  height: 300px;
+  margin: 0 auto;
+  margin-bottom: 40px;
 }
-
-/* 主内容区动画 */
-.fade-enter-active,
-.slide-left-enter-active,
-.slide-right-enter-active,
-.slide-top-enter-active,
-.slide-bottom-enter-active {
-  transition: 0.2s;
-}
-
-.fade-leave-active,
-.slide-left-leave-active,
-.slide-right-leave-active,
-.slide-top-leave-active,
-.slide-bottom-leave-active {
-  transition: 0.15s;
-}
-
-.fade-enter-from {
-  opacity: 0;
-}
-
-.fade-leave-to {
-  opacity: 0;
+.chart-container>div {
+  width: 100%;
+  height: 100%;
 }
 </style>
